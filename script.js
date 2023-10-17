f23cc6ac0bc1fdc119bea522a73597569dcc7454
Shabaya Charles <66180683+Shabaya254@users.noreply.github.com>
2023-10-17 14:29:45 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+hamburger = document.querySelector(".hamburger");
+            hamburger.onclick = function (){
+                navbar = document.querySelector(".navbar");
+                navbar.classList.toggle("active");
+            }
+            
+            
+            var swiper = new Swiper(".mySwiper", {
+              slidesPerView: 2,
+              spaceBetween: 30,
+              loop: true,
+              pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+              },
+              navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+              },
+            });
